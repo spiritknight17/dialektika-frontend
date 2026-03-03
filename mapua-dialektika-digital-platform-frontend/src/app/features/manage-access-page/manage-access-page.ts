405e0a7792fc5be09c60f44b08c4c259b4be5e1f
Barusu <payormatthewjoshguerrero@gmail.com>
2026-03-03 15:25:07 +0800

    Image upload for manage access finished

--- mapua-dialektika-digital-platform-frontend/src/app/features/manage-access-page/manage-access-page.ts
+++ mapua-dialektika-digital-platform-frontend/src/app/features/manage-access-page/manage-access-page.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from '../../core/header/header.component';
 import { ManageAccessPageComponent } from '../../shared/components/manage-access-page.component/manage-access-page.component';
 import { MainLabelComponent } from "../../shared/components/main-label/main-label";
 import { ManageAccessModel } from '../../shared/components/manage-access-page.component/manage-access-model';
+
 @Component({
   selector: 'app-manage-access-page',
   standalone: true,
@@ -15,4 +16,19 @@ import { ManageAccessModel } from '../../shared/components/manage-access-page.co
 export class ManageAccessPage {
   searchControl = new FormControl('');
   userList = new ManageAccessModel().userList;
+  imageUrl: string | null = null;
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        const result = reader.result;
+        if (typeof result === 'string') {
+          this.imageUrl = result;
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }
